ac88979791caade704711d2ddfa80ea6b89a4207
Alfredo Di Forti <adf.palermo@gmail.com>
2020-11-23 18:50:23 +0100

    Esercizio Completato

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        emails:[],
+    },
+    created() {
+        this.ten();
+
+        
+    },
+    methods: {
+        ten() {
+            
+            for (i = 0; i <= 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+            // handle success
+            this.emails.push(response.data.response);
+          })
+          .catch( (error) => {
+            // handle error
+            console.log(error);
+          }); 
+         }
+
+        },
+
+    },
+});
